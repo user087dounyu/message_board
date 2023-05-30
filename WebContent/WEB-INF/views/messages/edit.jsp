563b301c209c9236ee725a974ec679ec5e621228
masaya.mizutani <2023dounyukenshu.087@gmail.com>
2023-05-30 13:11:46 +0900

    DestroyServlet.javaを作成

--- WebContent/WEB-INF/views/messages/edit.jsp
+++ WebContent/WEB-INF/views/messages/edit.jsp
@@ -9,6 +9,18 @@
         </form>
 
         <p><a href="${pageContext.request.contextPath}/index">一覧に戻る</a></p>
+             <p><a href="#" onclick="confirmDestroy();">このメッセージを削除する</a></p>
+        <form method="POST" action="${pageContext.request.contextPath}/destroy">
+            <input type="hidden" name="_token" value="${_token}" />
+        </form>
+        <script>
+        function confirmDestroy() {
+            if(confirm("本当に削除してよろしいですか？")) {
+                document.forms[1].submit();
+            }
+        }
+        </script>
+
 
     </c:param>
 </c:import>
---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+
+            em.getTransaction().begin();
+            em.remove(m);       // データ削除
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("message_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
